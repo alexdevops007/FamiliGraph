5c973d2991a961e5a7a3e1284a2ad688b0dbe10f
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2024-01-04 19:33:55 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const cors = require("cors");
+const db = require("./config/db");
+const familyRoutes = require('./routes/familyRoutes');
+
+const config = require("./config");
+const helmet = require("helmet");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur l'api ProFedeMap");
+});
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    message: "Bienvenue sur l'api ProFedeMap",
+  });
+});
+
+// Routes
+app.use('/family', familyRoutes);
+
+// Gestion des erreurs
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Erreur serveur");
+});
+
+// Lancement du serveur
+app.listen(config.port, () =>
+  console.log(`Le serveur s'exécute sur le port ${config.port}`.bgBlue.bold)
+);
