3c6a21bc3521d9024378dfc90d2d652a6c1260b6
Alex Simisi Nta <smsxander@gmail.com>
2024-01-06 16:27:41 +0100

    install package

--- controllers/familyController.js
+++ controllers/familyController.js
@@ -1,17 +1,22 @@
-const Family = require('../models/familyModel');
+const Family = require("../models/familyModel");
 
 const familyController = {
   // Obtenir tous les membres de la famille
   getFamilyMembers: async (req, res) => {
     try {
-      const familyMembers = await Family.find();
+      const familyMembers = await Family.find(
+        {},
+        "firstName lastName gender isAlive"
+      );
       res.status(200).json(familyMembers);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur lors de la récupération des membres de la famille' });
+      res.status(500).json({
+        message: "Erreur lors de la récupération des membres de la famille",
+      });
     }
   },
-  
+
   // Créer un membre de la famille
   createFamilyMember: async (req, res) => {
     try {
@@ -19,7 +24,9 @@ const familyController = {
       res.status(201).json(newFamilyMember);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur lors de la création du membre de la famille' });
+      res.status(500).json({
+        message: "Erreur lors de la création du membre de la famille",
+      });
     }
   },
 
@@ -30,18 +37,24 @@ const familyController = {
       res.status(200).json(familyMember);
     } catch (error) {
       console.error(error);
-      res.status(404).json({ message: 'Membre de la famille non trouvé' });
+      res.status(404).json({ message: "Membre de la famille non trouvé" });
     }
   },
 
   // Mettre à jour un membre de la famille par ID
   updateFamilyMember: async (req, res) => {
     try {
-      const updatedFamilyMember = await Family.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      const updatedFamilyMember = await Family.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
       res.status(200).json(updatedFamilyMember);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur lors de la mise à jour du membre de la famille' });
+      res.status(500).json({
+        message: "Erreur lors de la mise à jour du membre de la famille",
+      });
     }
   },
 
@@ -52,7 +65,9 @@ const familyController = {
       res.status(204).end();
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Erreur lors de la suppression du membre de la famille' });
+      res.status(500).json({
+        message: "Erreur lors de la suppression du membre de la famille",
+      });
     }
   },
 };
