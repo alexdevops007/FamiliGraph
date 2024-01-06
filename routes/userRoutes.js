a6d9b446d7a4d33b2f50e07f4b5bfafbe962dc7d
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2024-01-06 16:59:27 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,18 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticationMiddleware = require('../middleware/authenticationMiddleware');
 
-// Routes pour la gestion des utilisateurs
-
-// Route protégée nécessitant une authentification (exemple)
-router.get('/protected', authenticationMiddleware.verifyToken, (req, res) => {
-  res.status(200).json({ message: 'Ceci est une route protégée' });
-});
-
-// Routes publiques (aucune authentification requise)
-router.post('/signup', userController.createUser);
-router.post('/login', authenticationController.login);
-
 // Routes nécessitant une authentification
+router.use(authenticationMiddleware.verifyToken);
 router.use(authenticationMiddleware.authenticateUser);
 
 router.get('/users', userController.getUsers);
